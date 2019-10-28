fb9278d47994974e172d479bc922ef36f49086a0
David Acosta <davidacosta25081@yahoo.com>
2019-10-28 02:21:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ function updateDriverWithKeyAndValue(obj,key,value)  {
 return Object.assign({} , obj, {[key]: value });
 }
 
-const driver = {nationality : "indian"};
 
-const newDriver = updateDriverWithKeyAndValue(driver,'nationality','colombian');
+
+
+
+destructivelyUpdateDriverWithKeyAndValue(obj,key,value) {
+  return Object.assign( obj, {[key]: value });
