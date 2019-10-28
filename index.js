0f3890fa752eec41f6de9f5a86a15e4567652a6e
David Acosta <davidacosta25081@yahoo.com>
2019-10-28 02:22:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ return Object.assign({} , obj, {[key]: value });
 
 
 
-destructivelyUpdateDriverWithKeyAndValue(obj,key,value) {
+function destructivelyUpdateDriverWithKeyAndValue(obj,key,value) {
   return Object.assign( obj, {[key]: value });
 }
